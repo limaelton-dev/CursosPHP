f87232b501955525a8d45e8b6e816830fb927173
Elton Lima <eltonlima.contato@gmail.com>
2022-12-08 07:58:44 -0300

    comecando a criar tabela relacional de telefones

--- PHP e PDO/conexao.php
+++ PHP e PDO/conexao.php
@@ -6,4 +6,21 @@ $pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo "Conectado";
 
-$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
+$createTableSql = "
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_date TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY(student_id) REFERENCES students(id)
+    );  
+";
+//FOREIGN KEY(student_id) REFERENCES students(id) , obriga a só poder criar um telefone, se ele fizer referencia à um aluno(id), se não, não cria
+
+$pdo->exec($createTableSql);
