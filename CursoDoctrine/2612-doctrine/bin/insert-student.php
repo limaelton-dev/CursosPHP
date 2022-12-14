bb19c04f77f4799b4327edff2957370c789269e1
Elton Lima <eltonlima.contato@gmail.com>
2022-12-14 07:41:29 -0300

    Criado relacionamento entre Student e Phones

--- CursoDoctrine/2612-doctrine/bin/insert-student.php
+++ CursoDoctrine/2612-doctrine/bin/insert-student.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alura\Doctrine\Entity\Phone;
 use Alura\Doctrine\Entity\Student;
 use Alura\Doctrine\Helper\EntityManagerCreator;
 
@@ -7,7 +8,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
 
-$student = new Student($argv[1]); //argv é uma variável reservada pelo doctrine, para passar dados atraves da linha de comando
+//$student = new Student($argv[1]); //argv é uma variável reservada pelo doctrine, para passar dados atraves da linha de comando
+
+$phone1 = new Phone('(42)9 9999-9999');
+$phone2 =new Phone('(42)9 3223-9999');
+$entityManager->persist($phone1);
+$entityManager->persist($phone2);
+
+$student = new Student('Aluno com telefones');
+$student->addPhone($phone1);
+$student->addPhone($phone2);
 
 $entityManager->persist($student); //esse metodo armazena o parametro, para ser enviado depois
 $entityManager->flush(); //manda tudo pro banco de dados
--- CursoDoctrine/2612-doctrine/src/Entity/Student.php
+++ CursoDoctrine/2612-doctrine/src/Entity/Student.php
@@ -2,11 +2,13 @@
 
 namespace Alura\Doctrine\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
-
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity]
 class Student
@@ -16,11 +18,25 @@ class Student
     #[Column] //aqui informo que o $id vai ser uma coluna do BD
     public int $id;
 
+    #[OneToMany(targetEntity: Phone::class, mappedBy: "student")] //um(aluno) para muitos(telefones), assim defino relacionamento
+    //preciso informar o tagetEntity e relacionar e com PHONE, depois o mappedBy para o atributo $student, da classe Phone
+    private Collection $phones; //iterable, qualquer coisa que possa fazer um foreach
+
     public function __construct(
         #[Column]  //aqui to informando que o nome vai ser uma coluna
         public string $name
     ) {
-
+        $this->phones = new ArrayCollection();
     }
 
+    public function addPhone(Phone $phone)
+    {
+        $this->phones->add($phone); //adiciono telefone
+        $phone->setStudent($this); //defino lá no telefone, que é este o aluno em questão
+    }
+
+    public function phones(): iterable
+    {
+        return $this->phones;
+    }
 }
