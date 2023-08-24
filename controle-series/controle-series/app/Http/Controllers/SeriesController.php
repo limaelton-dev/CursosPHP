1906bd0997bd5220fc69c977c8261e9ac5caa2c8
Elton Lima <eltonlima.contato@gmail.com>
2023-08-24 06:50:41 -0300

    criados metodos de insert e listagem de series

--- controle-series/controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/controle-series/app/Http/Controllers/SeriesController.php
@@ -6,13 +6,9 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = [
-            'Punisher',
-            'Lost',
-            'The Witcher'
-        ];
+        $series = \DB::select('SELECT nome FROM series');
 
         return view('series.index')->with('series', $series);
     }
@@ -20,5 +16,15 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        if(\DB::insert('INSERT INTO series (nome) VALUES(?)', [$nomeSerie])) {
+            return 'OK';
+        } else {
+            return 'Não deu boa';
+        }
     }
 }
--- controle-series/controle-series/resources/views/series/index.blade.php
+++ controle-series/controle-series/resources/views/series/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout title="Séries">
     <ul class="list-group">
         @foreach ($series as $serie)
-        <li class="list-group-item">{{ $serie }}</li>
+        <li class="list-group-item">{{ $serie->nome }}</li>
         @endforeach
         
     </ul>
