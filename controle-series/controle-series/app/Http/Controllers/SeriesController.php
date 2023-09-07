b7ae92c46a4f26099152094efccc9ff4f973b596
Elton Lima <eltonlima.contato@gmail.com>
2023-09-07 10:37:53 -0300

    colocando validate

--- controle-series/controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/controle-series/app/Http/Controllers/SeriesController.php
@@ -24,6 +24,10 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'nome' => ['required', 'min:3']
+        ]);
+
         $serie = Serie::create($request->all());
 
         return to_route('series.index')->with('mensagem.sucesso', "Série {$serie->nome} criada com sucesso!");
