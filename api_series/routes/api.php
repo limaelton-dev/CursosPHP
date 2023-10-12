833b4172125213ce07dde4ac69e0a73115a9e0b6
Elton Lima <eltonlima.contato@gmail.com>
2023-10-12 17:56:18 -0300

    adicionando recursos de autenticacao e finalizando API

--- api_series/routes/api.php
+++ api_series/routes/api.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\Api\SeriesController;
 use App\Http\Controllers\ControllerApi;
 use App\Models\Episode;
 use App\Models\Series;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,18 +25,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series', SeriesController::class);
-Route::get('/series/{series}/seasons', function(Series $series) {
-    return $series->seasons;
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', SeriesController::class);
+    Route::get('/series/{series}/seasons', function(Series $series) {
+        return $series->seasons;
+    });
+    
+    Route::get('/series/{series}/episodes', function(Series $series) {
+        return $series->episodes;
+    });
+    
+    Route::patch('/episodes/{episode}', function(Episode $episode, Request $request) {
+        $episode->watched = $request->watched;
+        $episode->save();
+    
+        return $episode;
+    });
 });
 
-Route::get('/series/{series}/episodes', function(Series $series) {
-    return $series->episodes;
-});
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if(Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
 
-Route::patch('/episodes/{episode}', function(Episode $episode, Request $request) {
-    $episode->watched = $request->watched;
-    $episode->save();
-
-    return $episode;
+    $user = Auth::user();
+    $token = $user->createToken('token');
+    
+    return response()->json($token->plainTextToken);
 });
